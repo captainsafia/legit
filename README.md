880eb4d406e74b315f231c8f9f44ec26b48578a1
Maurizio Pillitu <maoo@symphony.foundation>
2017-03-03 12:36:40 +0100

    remove hardcoded lists of placeholders; readme updated

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ npm install --global @captainsafia/legit
 ### Usage
 
 ```
-  Usage: legit [options]
+  Usage: legit [options] [placeholders]
 
   Options:
 
@@ -20,20 +20,33 @@ npm install --global @captainsafia/legit
     -V, --version            output the version number
     -a, --list-all           List installed licenses
     -l, --license            The license to include, as an SPDX License Identifier (mandatory)
-    -u, --user <user>        The individual who owns the IP (mandatory)
-    -y, --year <year>        The year the license is effective (optional, defaults to this year)
-    -n, --name <name>        A short name for the package (mandatory)
-    -w, --webpage <URL>      The URL of the home of the package e.g. on GitHub (optional, defaults to NONE)
+
+  Placeholders:
+    <key>=<value>
+```
+
+### Example
+```
+mkdir /tmp/test ; cd $_
+legit -l Apache-2.0 year=2001 user=maoo name=legit package-home-page=https://github.com/maoo/legit
+```
+
+If not `year` is the defined, the current one is used.
+
+Placeholder keys are resolved against [license-placeholders.yml](license-placeholders.yml) definition, depending on the license in use.
+
+If no placeholders configuration is available for the requested license, legit will prepend the following license header:
+```
+Copyright (c) [year] [user]
+
 
 ```
 
-![Legit Demo](legit-demo.gif)
-
-### Available Licenses
+### Supported Licenses
 
 legit uses SPDX license IDs and definitions as a backend, therefore all [SPDX licenses](https://spdx.org/licenses/) are supported.
 
-Additionally, it allows to replace placeholders in the license text with command-line options; placeholders are defined by [license-placeholders.yml](license-placeholders.yml):
+[license-placeholders.yml](license-placeholders.yml) defines placeholder mappings for the following licenses:
 
 - [AGPL-3.0](https://spdx.org/licenses/AGPL-3.0.html)
 - [Apache-2.0](https://spdx.org/licenses/Apache-2.0.html)
@@ -43,16 +56,7 @@ Additionally, it allows to replace placeholders in the license text with command
 - [MPL-2.0](https://spdx.org/licenses/MPL-2.0.html)
 - [Unlicense](https://spdx.org/licenses/Unlicense.html)
 
-If no placeholders are available, legit will prepend the following license header:
-```
-Copyright (c) [year] [user]
-
-
-```
-
 ### Known issues
 
-- placeholder list is hardcoded (`user`,`year`,`name`), should be parametric
-- add more items in [license-placeholders.yml](license-placeholders.yml)
 - placeholders including `'` character don't work
 - Regexp support for license placeholders
--- index.js
+++ index.js
@@ -32,18 +32,24 @@ program
   .usage('[options]')
   .option('-a, --list-all', 'List all SPDX license identifiers')
   .option('-l, --license <license>', 'The license to include, as an SPDX license identifier (mandatory)', validateLicense)
-  .option('-u, --user <user>', 'The individual who owns the IP (mandatory)')
-  .option('-y, --year <year>', 'The year the license is effective (optional, defaults to this year)')
-  .option('-n, --name <name>', 'A short name of the package (mandatory)')
-  .option('-w, --webpage <URL>', 'The URL of the home of the package e.g. on GitHub (optional, defaults to NONE)')
   .parse(process.argv);
 
-if (program.license && program.user && program.name) {
+console.log(program.args);
+
+if (program.license) {
   const cwd = process.cwd();
 
+  //Create a placeholders hash
+  var placeholders = {}
+  program.args.forEach(function(placeholder) {
+    keyValue = placeholder.split("=");
+    placeholders[keyValue[0]] = keyValue[1];
+  });
+
   // Populate defaults, if needed
-  program.year    = program.year    || (new Date().getFullYear()).toString();
-  program.webpage = program.webpage || "NONE";  // See https://spdx.org/spdx-specification-21-web-version#h.3o7alnk
+  placeholders['spdx-license-identifier'] = program.license;
+  placeholders['year']    = placeholders['year']    || (new Date().getFullYear()).toString();
+  placeholders['webpage'] = placeholders['webpage'] || "NONE";  // See https://spdx.org/spdx-specification-21-web-version#h.3o7alnk
 
   // Fetch license text from official SPDX license list on GitHub
   var licenseUrl = 'https://raw.githubusercontent.com/spdx/license-list/master/' + program.license + '.txt';
@@ -61,24 +67,27 @@ if (program.license && program.user && program.name) {
 
       var parsedLicenseText = licenseText;
 
-      placeholders = licensePlaceholders[program.license];
+      placeholderTokens = licensePlaceholders[program.license];
 
       // Define default license header
-      if (!placeholders) {
+      if (!placeholderTokens) {
         parsedLicenseText = "Copyright (c) [year] [user]\n\n"+parsedLicenseText;
-        placeholders = [{
+        placeholderTokens = [{
           "user": '[user]',
           "year": '[year]'
         }]
       }
 
-      placeholders.forEach(function(placeholder) {
-        Object.keys(placeholder).forEach(function(placeholderKey) {
-          var placeholderOldValue = placeholder[placeholderKey];
-          var placeholderNewValue = program[placeholderKey];
-          console.debug('Replacing ' + placeholderOldValue + ' with ' + placeholderNewValue + ' in LICENSE text');
-
-          parsedLicenseText = replaceAll(placeholderOldValue, placeholderNewValue, parsedLicenseText);
+      placeholderTokens.forEach(function(placeholderItem) {
+        Object.keys(placeholderItem).forEach(function(placeholderKey) {
+          var placeholderToken = placeholderItem[placeholderKey];
+          var placeholderValue = placeholders[placeholderKey];
+          console.log('Replacing ' + placeholderToken + ' with ' + placeholderValue + ' in LICENSE text');
+          if (placeholderValue && parsedLicenseText.indexOf(placeholderToken) > -1) {
+            parsedLicenseText = replaceAll(placeholderToken, placeholderValue, parsedLicenseText);
+          } else {
+            console.log("WARNING! Couldn't read placeholder '"+ placeholderToken +"' from command-line params");
+          }
         });
       });
 
@@ -96,10 +105,16 @@ if (program.license && program.user && program.name) {
       process.exit(1);
     } else {
       var spdxLicenseText = data;
-      spdxLicenseText = replaceAll('[user]',                    program.user,    spdxLicenseText);
-      spdxLicenseText = replaceAll('[name]',                    program.name,    spdxLicenseText);
-      spdxLicenseText = replaceAll('[package-home-page]',       program.webpage, spdxLicenseText);
-      spdxLicenseText = replaceAll('[spdx-license-identifier]', program.license, spdxLicenseText);
+      Object.keys(placeholders).forEach(function(placeholderKey) {
+        var placeholderValue = placeholders[placeholderKey];
+        var placeholderToken = '['+placeholderKey+']';
+        if (placeholderValue && spdxLicenseText.indexOf(placeholderToken) > -1) {
+          spdxLicenseText = replaceAll(placeholderToken, placeholderValue, spdxLicenseText);
+        } else {
+          console.log("WARNING! Couldn't read placeholder '"+ placeholderKey +"' from command-line params");
+        }
+      });
+
       fs.writeFile(cwd + '/LICENSE.spdx', spdxLicenseText, 'utf8', function (error) {
         if (error) {
           console.error(error);
