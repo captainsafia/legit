41f462d72c3c1df1deeb44a7e944afc4a56e9a66
Safia Abdalla <safia@safia.rocks>
2017-03-03 17:54:12 -0800

    Add custom definitions for placeholders

---
+++ placeholders.js
@@ -0,0 +1,36 @@
+const placeholders = {
+  'agpl-3.0': {
+    user: '<name of author',
+    year: '<year>'
+  },
+  'apache-2.0': {
+    user: '{name of copyright owner}',
+    year: '{yyyy}'
+  },
+  'bsd-2-clause': {
+    user: '[fullname]',
+    year: '[year]'
+  },
+  'bsd-3-clause': {
+    user: '[fullname]',
+    year: '[year]'
+  },
+  'gpl-2.0': {
+    user: '{fullname}',
+    year: '{year}'
+  },
+  'gpl-3.0': {
+    user: '{fullname}',
+    year: '{year}'
+  },
+  'lgpl-2.1': {
+    user: '{fullname}',
+    year: '{year}', 
+  },
+  'mit': {
+    user: '[fullname]',
+    year: '[year]'
+  }
+}
+
+module.exports = placeholders;
